dd4e5d44d99fe2518a9c55e46a9d0e7c0f278f21
Tazo <tazogigitashvili@gmail.com>
2025-05-18 14:30:51 +0400

    Initial commit: Create SwiftUI Project with .gitignore

---
+++ MovieExplorer/MovieExplorer/MovieExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieExplorerApp.swift
+//  MovieExplorer
+//
+//  Created by Tazo Gigitashvili on 18.05.25.
+//
+
+import SwiftUI
+
+@main
+struct MovieExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MovieExplorer/MovieExplorer/Services/MovieService.swift
@@ -0,0 +1,29 @@
+//
+//  MovieService.swift
+//  MovieExplorer
+//
+//  Created by Tazo Gigitashvili on 18.05.25.
+//
+
+import Foundation
+
+class MovieService {
+    private let apiKey = "1a96a69f1ad40a2dde00e71900241f7d"
+
+    func fetchPopularMovies() async throws -> [Movie] {
+        let urlString = "https://api.themoviedb.org/3/movie/popular?api_key=\(apiKey)&language=en-US&page=1"
+        guard let url = URL(string: urlString) else {
+            throw URLError(.badURL)
+        }
+
+        let (data, response) = try await URLSession.shared.data(from: url)
+
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            throw URLError(.badServerResponse)
+        }
+
+        let decoded = try JSONDecoder().decode(MovieResponse.self, from: data)
+        return decoded.results
+    }
+}
+
